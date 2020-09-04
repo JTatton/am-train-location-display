fd97aa5bbc148e42d0c17f884b15c3a5a425c5d1
Josh Tatton <7052428+JTatton@users.noreply.github.com>
2020-09-04 09:56:11 +0930

    Some unknown changes

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from google.transit import gtfs_realtime_pb2
 import urllib.request
 import matplotlib.pyplot as plt
@@ -19,10 +21,24 @@ lims =  [138.4360, 138.7971, -35.2075, -34.5676]
 ax.set_xlim(lims[0],lims[1])
 ax.set_ylim(lims[2],lims[3])
 
-feed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.request.urlopen('https://gtfs.adelaidemetro.com.au/v1/realtime/vehicle_positions')
-feed.ParseFromString(response.read())
-while True:
+#feed = gtfs_realtime_pb2.FeedMessage()
+#response = urllib.request.urlopen('https://gtfs.adelaidemetro.com.au/v1/realtime/vehicle_positions')
+#feed.ParseFromString(response.read())
+
+#img = plt.imread("map.png")
+#ax.imshow(img, zorder=0, extent = lims, aspect= 'equal')
+
+while(True):
+
+    feed = gtfs_realtime_pb2.FeedMessage()
+    response = urllib.request.urlopen('https://gtfs.adelaidemetro.com.au/v1/realtime/vehicle_positions')
+    feed.ParseFromString(response.read())
+
+    img = plt.imread("map.png")
+    ax.imshow(img, zorder=0, extent = lims, aspect= 'equal')
+    longs = []
+    lats = []
+
     for entity in feed.entity:
         if entity.HasField('vehicle'):
             if entity.vehicle.vehicle.id.startswith("40") or entity.vehicle.vehicle.id.startswith("30") or entity.vehicle.vehicle.id.startswith("31"):
@@ -38,33 +54,33 @@ while True:
                 longs.append(longitude)
                 lats.append(latitude)
 
-                #print("ID: " + str(veh_id))
-                #print("Longitude: " + str(longitude))
-                #print("New Longitude: " + str(vector_long))
-                #print("Latitude: " + str(latitude))
-                #print("New Latitude: " + str(vector_lat))
-                #print("Bearing: " + str(bearing))
-                #print("Speed: " + str(speed))
-                #print()
-
-                ax.arrow(longitude, latitude, vector_long, vector_lat, head_width=0.005, head_length=0.01, fc='k', ec='k')
+                #ax.arrow(longitude, latitude, vector_long, vector_lat, head_width=0.005, head_length=0.01, fc='k', ec='k')
                 ax.annotate(str(veh_id), (longitude, latitude))
-                
-
-
-    img = plt.imread("map.png")
-
-
-    ax.scatter(longs, lats, zorder=1, alpha= 1, c='b', s=20)
-    #ax.arrow(longs,lats,long_new,lat_new, head_width=0.05, head_length=0.1, fc='k', ec='k')
-
-
-    ax.imshow(img, zorder=0, extent = lims, aspect= 'equal')
-
-    #plt.show()
-    plt.draw() 
-    plt.pause(0.01)
-    print("Start Sleep")
+                scat= ax.scatter(longs, lats, zorder=1, alpha= 1, c='b', s=20)
+                plt.draw()
+    
+    plt.show()
+    print("Start Pause")
     plt.pause(20)
-    print("End Sleep")
-    plt.clf()
+    print("End pause")
+    scat.remove()
+    plt.draw()
+    plt.show()
+
+
+#img = plt.imread("map.png")
+
+
+#ax.scatter(longs, lats, zorder=1, alpha= 1, c='b', s=20)
+#ax.arrow(longs,lats,long_new,lat_new, head_width=0.05, head_length=0.1, fc='k', ec='k')
+
+#img = plt.imread("map.png")
+#ax.imshow(img, zorder=0, extent = lims, aspect= 'equal')
+
+#plt.show()
+#plt.draw() 
+#plt.pause(0.01)
+#print("Start Sleep")
+#plt.pause(20)
+#print("End Sleep")
+#plt.clf()
