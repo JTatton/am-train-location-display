0ffc453a6ec58dfd98f22ee53f772eba8fb2f7f4
Josh Tatton <7052428+JTatton@users.noreply.github.com>
2020-11-20 14:30:23 +1030

    remove all but outha

--- leds.py
+++ leds.py
@@ -20,29 +20,29 @@ LED_INVERT = 0
 LED_CHANNEL = 0
 
 outhaStrip = Adafruit_NeoPixel(OUTHA_COUNT, OUTHA_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-gawStrip = Adafruit_NeoPixel(GAW_COUNT, GAW_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-seafStrip = Adafruit_NeoPixel(SEAF_COUNT, SEAF_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-belStrip = Adafruit_NeoPixel(BEL_COUNT, BEL_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+#gawStrip = Adafruit_NeoPixel(GAW_COUNT, GAW_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+#seafStrip = Adafruit_NeoPixel(SEAF_COUNT, SEAF_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+#belStrip = Adafruit_NeoPixel(BEL_COUNT, BEL_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 
 def setupStrips():
     outhaStrip.begin()
-    gawStrip.begin()
-    seafStrip.begin()
-    belStrip.begin()
+ #   gawStrip.begin()
+  #  seafStrip.begin()
+   # belStrip.begin()
 
 def light(line, stop):
     if line == "OUTHA":
         outhaStrip.setPixelColor(stop, Color(255,0,0))
         outhaStrip.show()
     elif line == "GAW":
-        gawStrip.setPixelColor(stop, Color(255,0,0))
-        gawStrip.show()
+    #    gawStrip.setPixelColor(stop, Color(255,0,0))
+     #   gawStrip.show()
     elif line == "SEAF":
-        seafStrip.setPixelColor(stop, Color(255,0,0))
-        seafStrip.show()
+      #  seafStrip.setPixelColor(stop, Color(255,0,0))
+       # seafStrip.show()
     elif line == "BEL":
-        belStrip.setPixelColor(stop, Color(255,0,0))
-        belStrip.show()
+        #belStrip.setPixelColor(stop, Color(255,0,0))
+        #belStrip.show()
     else:
         print("Incorrect use of line. Use; OUTHA, GAW, SEAF, BEL")
 
@@ -52,14 +52,14 @@ def clear():
         outhaStrip.show()
         time.sleep(50/1000.0)
     for i in range(GAW_COUNT):
-        gawStrip.setPixelColor(i, Color(0,0,0))
-        gawStrip.show()
+      #  gawStrip.setPixelColor(i, Color(0,0,0))
+      #  gawStrip.show()
         time.sleep(50/1000.0)
     for i in range(SEAF_COUNT):
-        seafStrip.setPixelColor(i, Color(0,0,0))
-        seafStrip.show()
+       # seafStrip.setPixelColor(i, Color(0,0,0))
+       # seafStrip.show()
         time.sleep(50/1000.0)
     for i in range(BEL_COUNT):
-        belStrip.setPixelColor(i, Color(0,0,0))
-        belStrip.show()
+        #belStrip.setPixelColor(i, Color(0,0,0))
+        #belStrip.show()
         time.sleep(50/1000.0)
