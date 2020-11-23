ffab8914dd3493181b824576e0ab4dfb745c6cf7
Josh Tatton <7052428+JTatton@users.noreply.github.com>
2020-11-23 11:30:46 +1030

    added global keyword

--- am-display.py
+++ am-display.py
@@ -47,6 +47,7 @@ def distance(x1,y1,x2,y2):
     return math.sqrt( (x2-x1) ** 2 + (y2-y1) ** 2 )
 
 def setLights():
+    global trains
     prevTrains = trains
     trains = [[]]
     adelaideMetroFeed = downloadGTFSFeed()
