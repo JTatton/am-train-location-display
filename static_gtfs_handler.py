f8d2e72f050318de8208b6a02c52a084d56e98d6
JTatton <7052428+JTatton@users.noreply.github.com>
2023-08-10 11:30:06 +0930

    Create Static Handler

---
+++ static_gtfs_handler.py
@@ -0,0 +1,24 @@
+"""
+Module for handling Static GTFS Feed Data
+
+Downloads and Extracts Zip
+Imports txts as CSVs for:
+    - agency.txt            -- TO DO
+    - calendar_dates.txt    -- TO DO
+    - calendar.txt          -- TO DO
+    - feed_info.txt         -- TO DO
+    - Release Notes.txt     -- TO DO
+    - routes.txt            -- TO DO
+    - shapes.txt            -- TO DO
+    - stop_times.txt        -- TO DO
+    - stops.txt             -- TO DO
+    - transfers.txt         -- TO DO
+    - trips.txt             -- TO DO
+"""
+
+def main():
+    """Main should only call when testing"""
+    print("Static GTFS Handler")
+
+if __name__ == "__main__":
+    main()
