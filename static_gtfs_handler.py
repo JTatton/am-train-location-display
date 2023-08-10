2c15dd99ed0e1a7464222c6e6012a4f7fa559091
JTatton <7052428+JTatton@users.noreply.github.com>
2023-08-10 14:31:13 +0930

    Implemented static gtfs handler

--- static_gtfs_handler.py
+++ static_gtfs_handler.py
@@ -11,7 +11,7 @@ Imports txts as CSVs for:
     - routes.txt            -- TO DO
     - shapes.txt            -- TO DO
     - stop_times.txt        -- TO DO
-    - stops.txt             -- TO DO
+    - stops.txt
     - transfers.txt         -- TO DO
     - trips.txt             -- TO DO
 """
@@ -19,6 +19,20 @@ from io import BytesIO
 import zipfile
 import csv
 import requests
+
+class FeedInfo:
+    """FeedInfo Class
+        - Stores infomation about the currency of the feed"""
+    def __init__(self,feed):
+        self.feed_publisher_name = feed["feed_publisher_name"]
+        self.feed_publisher_url = feed["feed_publisher_url"]
+        self.feed_lang = feed["feed_lang"]
+        self.feed_start_date = feed["feed_start_date"]
+        self.feed_end_date = feed["feed_end_date"]
+        self.feed_version = feed["feed_version"]
+
+    def get_feed_end_date(self):
+        return self.feed_end_date
 
 class Stop:
     """Stop class"""
@@ -50,7 +64,85 @@ class Stop:
     
     def get_stop(self):
         return (self.stop_id, self.stop_code, self.stop_name, self.stop_desc, self.stop_lat, self.stop_lon, self.zone_id, self.stop_url, self.location_type, self.parent_station, self.stop_timezone, self.wheelchair_boarding)
+
+class StopTime():
+    """Stop Time Class"""
+    def __init__(self,stop_time):
+        self.trip_id = stop_time["trip_id"]
+        self.arrival_time = stop_time["arrival_time"]
+        self.departure_time = stop_time["departure_time"]
+        self.stop_id = stop_time["stop_id"]
+        self.stop_sequence = stop_time["stop_sequence"]
+        self.stop_headsign = stop_time["stop_headsign"]
+        self.pickup_type = stop_time["pickup_type"]
+        self.drop_off_type = stop_time["drop_off_type"]
+        self.shape_dist_traveled = stop_time["shape_dist_traveled"]
+        self.timepoint = stop_time["timepoint"]
+
+    def get_stop_id(self):
+        return self.stop_id
+
+class Trip():
+    def __init__(self, trip):
+        self.route_id = trip["route_id"]
+        self.service_id = trip["service_id"]
+        self.trip_id = trip["trip_id"]
+        self.trip_headsign = trip["trip_headsign"]
+        self.trip_short_name = trip["trip_short_name"]
+        self.direction_id = trip["direction_id"]
+        self.block_id = trip["block_id"]
+        self.shape_id = trip["shape_id"]
+        self.wheelchair_accessible = trip["wheelchair_accessible"]
+
+    def get_route_id(self):
+        return self.route_id
     
+    def get_trip_id(self):
+        return self.trip_id
+
+class Route():
+    def __init__(self, route):
+        self.route_id = route["route_id"]
+        self.agency_id = route["agency_id"]
+        self.route_short_name = route["route_short_name"]
+        self.route_long_name = route["route_long_name"]
+        self.route_desc = route["route_desc"]
+        self.route_type = route["route_type"]
+        self.route_url = route["route_url"]
+        self.route_color = route["route_color"]
+        self.route_text_color = route["route_text_color"]
+        self.RouteGroup = route["RouteGroup"]
+
+    def get_route_id(self):
+        return self.route_id
+    
+    def get_route_color(self):
+        return self.route_color
+    
+    def get_route_colours_int(self):
+        r = "0x" + self.route_color[0:2]
+        g = "0x" + self.route_color[2:4]
+        b = "0x" + self.route_color[4:]
+
+        return (int(r,0),int(g,0),int(b,0))
+
+
+class Agency():
+    def __init__(self, agency):
+        self.agency_id = agency["agency_id"]
+        self.agency_name = agency["agency_name"]
+        self.agency_url = agency["agency_url"]
+        self.agency_timezone = agency["agency_timezone"]
+        self.agency_lang = agency["agency_lang"]
+        self.agency_phone = agency["agency_phone"]
+        self.agency_fare_url = agency["agency_fare_url"]
+
+def read_feed_info_csv():
+        with open("google-transit/feed_info.txt") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                return FeedInfo(row)
 
 def read_stop_csv():
     """Reads Stops to List of Stop Class"""
@@ -62,6 +154,47 @@ def read_stop_csv():
             stops.append(Stop(row))
 
     return stops
+
+
+def read_stop_times_csv():
+    stop_times = []
+
+    with open("google-transit/stop_times.txt") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            stop_times.append(StopTime(row))
+
+    return stop_times
+
+def read_trips_csv():
+    trips = []
+
+    with open("google-transit/trips.txt") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            trips.append(Trip(row))
+
+    return trips
+
+def read_routes_csv():
+    routes = []
+
+    with open("google-transit/routes.txt") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            routes.append(Route(row))
+
+    return routes
+
+def read_agency_csv():
+    agency = []
+
+    with open("google-transit/agency.txt") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            agency.append(Agency(row))
+
+    return agency
 
 def download_gt_zip(url):
     """Downloads google-transit zip and returns request object"""
@@ -99,10 +232,15 @@ def main():
 
     #extract_gt_zip(download_gt_zip(gtfs_zip_url))
 
+    feedinfo = read_feed_info_csv()
     stops = read_stop_csv()
+    stop_times = read_stop_times_csv()
+    trips = read_trips_csv()
+    routes = read_routes_csv()
+    agency = read_agency_csv()
 
-    for stop in stops:
-        print(f"ID: {stop.get_stop_id()}, Code: {stop.get_stop_code()}, Name: {stop.get_stop_name()}, Lat: {stop.get_stop_position()[0]}, Long: {stop.get_stop_position()[1]}")
+    for route in routes:
+        print(route.get_route_id(), route.get_route_color() ,route.get_route_colours_int())
 
 if __name__ == "__main__":
     main()
