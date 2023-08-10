a4f5fe8c38f1ce9fdf8054d988bfb999ad718dac
JTatton <7052428+JTatton@users.noreply.github.com>
2023-08-10 10:48:50 +0930

    Initial Working version of GTFSHandler

--- GTFSHandler.py
+++ GTFSHandler.py
@@ -1,8 +1,96 @@
 """
 Functions for handling fetching, parsing and dealing with the GTFS Feed
 """
-import leds
-from csv import reader
-from google.transit import gtfs_realtime_pb2
-import urllib.request
 import math
+from csv import reader
+from urllib.request import urlopen
+from google.transit import gtfs_realtime_pb2
+import leds
+
+FEED_URL = "https://gtfs.adelaidemetro.com.au/v1/realtime/vehicle_positions"
+
+class EntityVehicle:
+    """Vehicle in Entity"""
+    def __init__(self, entity):
+        self.vehicle_timestamp = entity.vehicle.timestamp
+
+        self.vehicle_trip = {}
+        self.vehicle_trip["trip_id"] = entity.vehicle.trip.trip_id
+        self.vehicle_trip["route_id"] = entity.vehicle.trip.route_id
+        self.vehicle_trip["direction_id"] = entity.vehicle.trip.direction_id
+        self.vehicle_trip["start_date"] = entity.vehicle.trip.start_date
+        self.vehicle_trip["schedule_relationship"] = entity.vehicle.trip.schedule_relationship
+
+        self.vehicle_vehicle = {}
+        self.vehicle_vehicle["id"] = entity.vehicle.vehicle.id
+        self.vehicle_vehicle["label"] = entity.vehicle.vehicle.label
+
+        self.vehicle_position = {}
+        self.vehicle_position["latitude"] = entity.vehicle.position.latitude
+        self.vehicle_position["longitude"] = entity.vehicle.position.longitude
+        self.vehicle_position["bearing"] = entity.vehicle.position.bearing
+        self.vehicle_position["speed"] = entity.vehicle.position.speed
+
+    def set_timestamp(self, timestamp):
+        """Setter Function for Timestamp"""
+        self.vehicle_timestamp = timestamp
+
+    def set_trip(self, trip_id, route_id, direction_id, start_date, schedule_relationship):
+        """Setter Function for Trip"""
+        self.vehicle_trip["trip_id"] = trip_id
+        self.vehicle_trip["route_id"] = route_id
+        self.vehicle_trip["direction_id"] = direction_id
+        self.vehicle_trip["start_date"] = start_date
+        self.vehicle_trip["schedule_relationship"] = schedule_relationship
+
+    def get_timestamp(self):
+        """Getter Function returns Timestamp"""
+        return self.vehicle_timestamp
+
+    def get_tripid(self):
+        """Getter Function returns Trip ID"""
+        return self.vehicle_trip["trip_id"]
+
+    def get_routeid(self):
+        """Getter Function returns Route ID"""
+        return self.vehicle_trip["route_id"]
+
+    def get_vehicleid(self):
+        """Getter Function returns Vehicle ID"""
+        return self.vehicle_vehicle["id"]
+
+    def get_position(self):
+        """Getter Function returns Tuple (latitude, longitude)"""
+        return (self.vehicle_position["latitude"], self.vehicle_position["longitude"])
+
+def main():
+    """Main should only be called during testing"""
+    print("Welcome to GTFSHandler.py")
+
+    print("Getting Feed")
+    gtfs_feed = get_feed()
+
+    entities = []
+
+    for entity in gtfs_feed.entity:
+        new_entity = EntityVehicle(entity)
+        #new_entity.set_trip(entity)
+        entities.append(new_entity)
+
+    for i in entities:
+        print(f"Timestamp: {i.get_timestamp()}, \
+              Route ID: {i.get_routeid()}, \
+              Vehicle ID: {i.get_vehicleid()}, \
+              Lat: {i.get_position()[0]}, \
+              Long: {i.get_position()[1]}")
+
+def get_feed():
+    """Function to get GTFS Feed and Parse to List"""
+    server_response = urlopen(FEED_URL)
+    gtfs_feed = gtfs_realtime_pb2.FeedMessage()
+    gtfs_feed.ParseFromString(server_response.read())
+    return gtfs_feed
+
+
+if __name__ == "__main__":
+    main()
